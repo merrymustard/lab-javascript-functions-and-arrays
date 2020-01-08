bb38fc4e7f95d0b297c03a35065ee811ac623fd2
merrymustard <merryymustard@gmail.com>
2020-01-08 10:03:15 -0600

    update

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,101 +1,676 @@
 // Find the maximum
-
+// function maxOfTwoNumbers(a, b) {
+//   if (a > b) {
+//     return a
+//   } else {
+//     return b
+//   }
+// }
+maxOfTwoNumbers = (num1, num2) => {
+  return Math.max(num1, num2)
+}
 // Finding Longest Word
 const words = [
-  'mystery',
-  'brother',
-  'aviator',
-  'crocodile',
-  'pearl',
-  'orchard',
-  'crackpot'
-];
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot"
+]
+findLongestWord = arr => {
+  let charCount = 0
+  let longestWord = null
+  arr.forEach(word => {
+    if (word.length > charCount) {
+      charCount = word.length
+      longestWord = word
+    }
+  })
+  return longestWord
+}
+
+findLongestWord(words)
 
 // Calculating a Sum
+const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10]
+sumArray = numbers => {
+  let num = 0
+  for (i = 0; i < numbers.length; i++) {
+    num += numbers[i]
+  }
+  return num
+}
 
-const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
-
+// sumArray = arr => {
+//   if (arr.length ===0) {
+//     return 0
+//   } else if(arr.length === 1) {
+//     return arr[0]
+//   }
+//   let suma = arr.reduce((accumulator, currentValue) => {
+//     return accumulator + currentValue
+//   })
+//   return suma
+// }
 // Calculate the Average
 
-const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9]
+function averageNumbers(numbers) {
+  if (numbers.length > 0) {
+    let num = 0
+    for (i = 0; i < numbers.length; i++) {
+      num += numbers[i]
+    }
+    return num / numbers.length
+  } else {
+    return null
+  }
+}
+
+// averageNumbers = (arr) => {
+//   let suma2 = (sum,current) => sum + current
+//   if (arr.length ===0) {
+//     return undefined
+//   } else {
+//   return arr.reduce(suma2) / arr.length
+//   }
+// }
+// averageNumbers(numbersAvg)
 
 // Array of Strings
 const wordsArr = [
-  'seat',
-  'correspond',
-  'linen',
-  'motif',
-  'hole',
-  'smell',
-  'smart',
-  'chaos',
-  'fuel',
-  'palace'
-];
+  "seat",
+  "correspond",
+  "linen",
+  "motif",
+  "hole",
+  "smell",
+  "smart",
+  "chaos",
+  "fuel",
+  "palace"
+]
 
+function averageWordLength(arr) {
+  if (arr.length > 0) {
+    return arr.join("").length / arr.length
+  } else {
+    return null
+  }
+}
+
+// averageWordLength = arr => {
+//   let charCount = 0
+// arr.forEach(i => {
+//   charCount = charCount + i.length
+// });
+// if (arr.length ===0) {
+//   return undefined
+// } else {
+//   return charCount / arr.length}
+// }
 // Unique Arrays
 const wordsUnique = [
-  'crab',
-  'poison',
-  'contagious',
-  'simple',
-  'bring',
-  'sharp',
-  'playground',
-  'poison',
-  'communion',
-  'simple',
-  'bring'
-];
+  "crab",
+  "poison",
+  "contagious",
+  "simple",
+  "bring",
+  "sharp",
+  "playground",
+  "poison",
+  "communion",
+  "simple",
+  "bring"
+]
+
+// uniquifyArray = arr => {
+//   let uniqueWords = []
+//   arr.forEach(i => {
+//     if (uniqueWords.indexOf(i) === -1) {
+//       uniqueWords.push(i)
+//     }
+//   })
+//   if (arr.length === 0) {
+//     return undefined
+//   } else {
+//     return uniqueWords
+//   }
+// }
+function uniquifyArray(arr) {
+  let newArray = []
+  if (arr.length > 0) {
+    for (let i = 0; i < arr.length; i++) {
+      if (newArray.includes(arr[i]) == false) {
+        newArray.push(arr[i])
+      } else {
+        continue
+      }
+    }
+    return newArray
+  } else {
+    return []
+  }
+}
 
 // Finding Elements
 const wordsFind = [
-  'machine',
-  'subset',
-  'trouble',
-  'starting',
-  'matter',
-  'eating',
-  'truth',
-  'disobedience'
-];
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience"
+]
 
+doesWordExist = (arr, word) => {
+  return arr.includes(word)
+}
+doesWordExist(wordsFind, "subset")
+console.log(doesWordExist(wordsFind, "fara"))
+
+// function doesWordExist(arr, word) {
+//   let check = false
+//   for (i = 0; i < arr.length; i++) {
+//     if (arr.includes(arr[i]) == word) {
+//       check = true
+//       return check
+//     }
+//   }
+//   return check
+// }
 // Counting Repetition
 const wordsCount = [
-  'machine',
-  'matter',
-  'subset',
-  'trouble',
-  'starting',
-  'matter',
-  'eating',
-  'matter',
-  'truth',
-  'disobedience',
-  'matter'
-];
+  "machine",
+  "matter",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "matter",
+  "truth",
+  "disobedience",
+  "matter"
+]
+function howManyTimes(arr, word) {
+  let counter = 0
+  arr.forEach(function(e, i, a) {
+    if (e == word) {
+      counter += 1
+    }
+  })
+  return counter
+}
 
+// howManyTimes = (arr, word) => {
+//   let timesWord = 0
+//   arr.forEach(i => {
+//     if (word === i) {
+//       timesWord++
+//     }
+//   })
+//   if (arr.length === 0) {
+//     return false
+//   } else {
+//     return timesWord
+//   }
+// }
+// howManyTimes(wordsCount, "matter")
 // Bonus
 
 const matrix = [
   [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
-  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
+  [
+    49,
+    49,
+    99,
+    40,
+    17,
+    81,
+    18,
+    57,
+    60,
+    87,
+    17,
+    40,
+    98,
+    43,
+    69,
+    48,
+    4,
+    56,
+    62,
+    0
+  ],
+  [
+    81,
+    49,
+    31,
+    73,
+    55,
+    79,
+    14,
+    29,
+    93,
+    71,
+    40,
+    67,
+    53,
+    88,
+    30,
+    3,
+    49,
+    13,
+    36,
+    65
+  ],
   [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
-  [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
-  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
-  [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
-  [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
+  [
+    22,
+    31,
+    16,
+    71,
+    51,
+    67,
+    63,
+    89,
+    41,
+    92,
+    36,
+    54,
+    22,
+    40,
+    40,
+    28,
+    66,
+    33,
+    13,
+    80
+  ],
+  [
+    24,
+    47,
+    32,
+    60,
+    99,
+    3,
+    45,
+    2,
+    44,
+    75,
+    33,
+    53,
+    78,
+    36,
+    84,
+    20,
+    35,
+    17,
+    12,
+    50
+  ],
+  [
+    32,
+    98,
+    81,
+    28,
+    64,
+    23,
+    67,
+    10,
+    26,
+    38,
+    40,
+    67,
+    59,
+    54,
+    70,
+    66,
+    18,
+    38,
+    64,
+    70
+  ],
+  [
+    67,
+    26,
+    20,
+    68,
+    2,
+    62,
+    12,
+    20,
+    95,
+    63,
+    94,
+    39,
+    63,
+    8,
+    40,
+    91,
+    66,
+    49,
+    94,
+    21
+  ],
+  [
+    24,
+    55,
+    58,
+    5,
+    66,
+    73,
+    99,
+    26,
+    97,
+    17,
+    78,
+    78,
+    96,
+    83,
+    14,
+    88,
+    34,
+    89,
+    63,
+    72
+  ],
   [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
   [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-  [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
+  [
+    16,
+    39,
+    5,
+    42,
+    96,
+    35,
+    31,
+    47,
+    55,
+    58,
+    88,
+    24,
+    0,
+    17,
+    54,
+    24,
+    36,
+    29,
+    85,
+    57
+  ],
   [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-  [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
+  [
+    19,
+    80,
+    81,
+    68,
+    5,
+    94,
+    47,
+    69,
+    28,
+    73,
+    92,
+    13,
+    86,
+    52,
+    17,
+    77,
+    4,
+    89,
+    55,
+    40
+  ],
   [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-  [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
-  [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
-  [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
-  [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
+  [
+    88,
+    36,
+    68,
+    87,
+    57,
+    62,
+    20,
+    72,
+    3,
+    46,
+    33,
+    67,
+    46,
+    55,
+    12,
+    32,
+    63,
+    93,
+    53,
+    69
+  ],
+  [
+    4,
+    42,
+    16,
+    73,
+    38,
+    25,
+    39,
+    11,
+    24,
+    94,
+    72,
+    18,
+    8,
+    46,
+    29,
+    32,
+    40,
+    62,
+    76,
+    36
+  ],
+  [
+    20,
+    69,
+    36,
+    41,
+    72,
+    30,
+    23,
+    88,
+    34,
+    62,
+    99,
+    69,
+    82,
+    67,
+    59,
+    85,
+    74,
+    4,
+    36,
+    16
+  ],
+  [
+    20,
+    73,
+    35,
+    29,
+    78,
+    31,
+    90,
+    1,
+    74,
+    31,
+    49,
+    71,
+    48,
+    86,
+    81,
+    16,
+    23,
+    57,
+    5,
+    54
+  ],
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
-];
+]
+function greatestProduct(matrix) {
+  let greatProd = { Position: [0, 0], Direction: "X", Product: 0 }
+
+  //Look up:
+  function calcProdUp(matrix, x, y) {
+    let prod =
+      matrix[x][y] * matrix[x][y - 1] * matrix[x][y - 2] * matrix[x][y - 3]
+    if (prod > greatProd["Product"]) {
+      greatProd["Product"] = prod
+      greatProd["Direction"] = "UP"
+      greatProd["Position"] = [x, y]
+    }
+  }
+  //Look down
+  function calcProdDown(matrix, x, y) {
+    let prod =
+      matrix[x][y] * matrix[x][y + 1] * matrix[x][y + 2] * matrix[x][y + 3]
+    if (prod > greatProd["Product"]) {
+      greatProd["Product"] = prod
+      greatProd["Direction"] = "DOWN"
+      greatProd["Position"] = [x, y]
+    }
+  }
+  //Look right
+  function calcProdRight(matrix, x, y) {
+    let prod =
+      matrix[x][y] * matrix[x + 1][y] * matrix[x + 2][y] * matrix[x + 3][y]
+    if (prod > greatProd["Product"]) {
+      greatProd["Product"] = prod
+      greatProd["Direction"] = "RIGHT"
+      greatProd["Position"] = [x, y]
+    }
+  }
+  //Look left
+  function calcProdLeft(matrix, x, y) {
+    let prod =
+      matrix[x][y] * matrix[x - 1][y] * matrix[x - 2][y] * matrix[x - 3][y]
+    if (prod > greatProd["Product"]) {
+      greatProd["Product"] = prod
+      greatProd["Direction"] = "LEFT"
+      greatProd["Position"] = [x, y]
+    }
+  }
+
+  // Diagonal up-right
+  function upRight(matrix, x, y) {
+    let prod =
+      matrix[x][y] *
+      matrix[x + 1][y - 1] *
+      matrix[x + 2][y - 2] *
+      matrix[x + 3][y - 3]
+    if (prod > greatProd["Product"]) {
+      greatProd["Product"] = prod
+      greatProd["Direction"] = "DIAGONAL UP-RIGHT"
+      greatProd["Position"] = [x, y]
+    }
+  }
+
+  // Diagonal down-right
+  function downRight(matrix, x, y) {
+    let prod =
+      matrix[x][y] *
+      matrix[x + 1][y + 1] *
+      matrix[x + 2][y + 2] *
+      matrix[x + 3][y + 3]
+    if (prod > greatProd["Product"]) {
+      greatProd["Product"] = prod
+      greatProd["Direction"] = "DIAGONAL UP-RIGHT"
+      greatProd["Position"] = [x, y]
+    }
+  }
+
+  //Diagonal down-left
+  function downLeft(matrix, x, y) {
+    let prod =
+      matrix[x][y] *
+      matrix[x - 1][y + 1] *
+      matrix[x - 2][y + 2] *
+      matrix[x + 3][y + 3]
+    if (prod > greatProd["Product"]) {
+      greatProd["Product"] = prod
+      greatProd["Direction"] = "DIAGONAL UP-RIGHT"
+      greatProd["Position"] = [x, y]
+    }
+  }
+
+  //Diagonal up-left
+  function upLeft(matrix, x, y) {
+    let prod =
+      matrix[x][y] *
+      matrix[x - 1][y - 1] *
+      matrix[x - 2][y - 2] *
+      matrix[x - 3][y - 3]
+    if (prod > greatProd["Product"]) {
+      greatProd["Product"] = prod
+      greatProd["Direction"] = "DIAGONAL UP-RIGHT"
+      greatProd["Position"] = [x, y]
+    }
+  }
+
+  //Check for all the box values:
+  for (x = 0; x < 20; x++) {
+    for (y = 0; y < 20; y++) {
+      try {
+        calcProdRight(matrix, x, y)
+        continue
+      } catch {
+        continue
+      }
+      try {
+        calcProdLeft(matrix, x, y)
+        continue
+      } catch {
+        continue
+      }
+      try {
+        calcProdUp(matrix, x, y)
+        continue
+      } catch {
+        continue
+      }
+      try {
+        calcProdDown(matrix, x, y)
+        continue
+      } catch {
+        continue
+      }
+      try {
+        upLeft(matrix, x, y)
+        continue
+      } catch {
+        continue
+      }
+      try {
+        downRight(matrix, x, y)
+        continue
+      } catch {
+        continue
+      }
+      try {
+        downLeft(matrix, x, y)
+        continue
+      } catch {
+        continue
+      }
+      try {
+        upRight(matrix, x, y)
+        continue
+      } catch {
+        continue
+      }
+    }
+  }
+  /*return console.log(`The greatest product is located in ${greatProd['Position']} looking ${greatProd['Direction']} with value of ${greatProd['Product']}`)*/
+  return greatProd.Product
+}
